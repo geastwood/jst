9d20b9a0d5d97a96024a0c128d79448b62ed6e9a
Fei Liu <liu.feiwood@gmail.com>
2015-02-16 22:49:24 +0100

    working in progress

--- jst.js
+++ jst.js
@@ -22,13 +22,13 @@ var getSuitName = function() {
 var getFileRealPath = function(filepath) {
     var fs = require('fs');
     var path = require('path');
-    var fullpath = path.resolve(__dirname, filepath);
+    var fullpath = path.resolve(process.cwd(), filepath);
     var defer = Q.defer();
     fs.exists(fullpath, function(exist) {
         if (exist) {
             defer.resolve(fullpath);
         } else {
-            defer.reject(new Error('file does not exist'));
+            defer.resolve(false);
         }
     });
     return defer.promise;
@@ -40,20 +40,42 @@ var getSuit = function(suit) {
     });
 };
 
+var runSuit = function(filepath, suit) {
+    var fs = require('fs'), tmp = require('os').tmpDir(), tmpFolder = String(Date.now()), path = require('path'),
+        wrench = require('wrench'), targetPath = path.join(tmp, tmpFolder);
+    var content = fs.readFileSync(filepath, 'utf8');
+    console.log(content);
+    //content = 'var _ = require(\'lodash\');' + '\n' + content;
+
+    wrench.copyDirSyncRecursive(path.join(__dirname, '/suit/capitalize/'), targetPath);
+    fs.writeFileSync(path.join(targetPath, 'check.js'), content);
+
+    var child = require('child_process').exec('nodeunit ' + path.join(targetPath, 'test'), function(stdin, stdout) {
+        console.log(stdout);
+        wrench.rmdirSyncRecursive(targetPath);
+    });
+};
+
 program.command('check <file>')
     .description('check a file against a suit')
     .option('-s --suit <suit>', 'specify suit')
     .action(function(file, options) {
-        Q.all([getFileRealPath(file), getSuit(options.suit)]).spread(function(file, suit) {
-            console.log(file, suit);
-        }).done();
 
-        //var content = fs.readFileSync(__dirname + '/' + options, 'utf8');
-        //fs.writeFileSync(__dirname + '/suit/capitalize/check.js', content);
-        //var child = require('child_process').exec('nodeunit suit/capitalize/test/', function(stdin, stdout) {
-        //    console.log(stdout);
-        //    fs.unlinkSync(__dirname + '/suit/capitalize/check.js');
-        //});
+        var fullFilePath;
+
+        // domain logic
+        getFileRealPath(file).then(function(filepath) {
+            if (filepath === false) {
+                console.log('fei');
+                throw new Error('File does not exist.');
+            }
+            fullFilePath = filepath;
+            return getSuit(options.suit);
+        }).then(function(suit) {
+            runSuit(fullFilePath, suit);
+        }).catch(function(err) {
+            console.log(err);
+        });
     });
 
 program.parse(process.argv);
